80a9fd613fd594ab885e5e4aca3be72042ba7274
admirkrilasevic <admirkrilasevic@gmail.com>
2022-01-12 21:45:23 +0100

    home page categories section test

--- src/test/HomePageTest.java
+++ src/test/HomePageTest.java
@@ -3,13 +3,16 @@ package test;
 import static org.junit.jupiter.api.Assertions.*;
 
 import java.time.Duration;
+import java.util.List;
 
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
@@ -28,6 +31,7 @@ class HomePageTest {
 		webDriver.manage().window().maximize();
 		wait = new WebDriverWait(webDriver, Duration.ofSeconds(10));
 		setup = new Setup(webDriver, wait);
+		setup.startServer();
 	}
 
 	@AfterAll
@@ -45,8 +49,36 @@ class HomePageTest {
 	}
 
 	@Test
-	void test() throws InterruptedException {
-
+	void testCategoriesSection() throws InterruptedException {
+		Thread.sleep(2000);
+		List<WebElement> categories = webDriver.findElements(By.className("CategoriesSection_categoryButton__2oY_0"));
+		
+		for (int i = 0; i < categories.size()-1; i++) {
+			Thread.sleep(2000);
+			
+			List<WebElement> refreshedCategories = webDriver.findElements(By.className("CategoriesSection_categoryButton__2oY_0"));
+			String categoryName = refreshedCategories.get(i).getText();
+			refreshedCategories.get(i).click();
+			
+			Thread.sleep(2000);
+			
+			WebElement categoryActiveFilter = webDriver.findElement(By.xpath("/html/body/div/div/div[3]/div/div/div[2]/div[1]/div/div/button"));
+			String categoryFilterName = categoryActiveFilter.getText().split(" ")[0];
+			assertEquals(categoryName, categoryFilterName);
+			
+			webDriver.navigate().back();
+		}
+		
+		Thread.sleep(2000);
+		List<WebElement> refreshedCategories = webDriver.findElements(By.className("CategoriesSection_categoryButton__2oY_0"));
+		refreshedCategories.get(refreshedCategories.size()-1).click();
+		String allCategoriesUrl = webDriver.getCurrentUrl();
+		
+		WebElement shopLink = webDriver.findElement(By.xpath("//*[@id=\"root\"]/div/div[2]/div/div[3]/a[2]"));
+		shopLink.click();
+		String shopUrl = webDriver.getCurrentUrl();
+		
+		assertEquals(shopUrl, allCategoriesUrl);
 	}
 
 }

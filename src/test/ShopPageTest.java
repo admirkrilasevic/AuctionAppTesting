22ea8cdb534ee6ef6bfb90dbbfe54bec6dd16608
admirkrilasevic <admirkrilasevic@gmail.com>
2022-01-13 23:39:56 +0100

    added category and subcategory filters test

--- src/test/ShopPageTest.java
+++ src/test/ShopPageTest.java
@@ -3,13 +3,17 @@ package test;
 import static org.junit.jupiter.api.Assertions.*;
 
 import java.time.Duration;
+import java.util.List;
 
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.NoSuchElementException;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
@@ -28,6 +32,7 @@ class ShopPageTest {
 		webDriver.manage().window().maximize();
 		wait = new WebDriverWait(webDriver, Duration.ofSeconds(10));
 		setup = new Setup(webDriver, wait);
+		setup.startServer();
 	}
 
 	@AfterAll
@@ -45,8 +50,68 @@ class ShopPageTest {
 	}
 
 	@Test
-	void test() throws InterruptedException {
-
+	void testCategoryFilters() throws InterruptedException {
+		openShopPage();
+		
+		List<WebElement> categories = webDriver.findElements(By.className("Category_collapsibleCategory__3wERX"));
+		
+		for (int i = 0; i < categories.size(); i++) {
+			WebElement currentCategory = categories.get(i);
+			String categoryName = currentCategory.getText().split("\\s+")[0];
+			currentCategory.click();
+			
+			WebElement categoryActiveFilter = webDriver.findElement(By.xpath("/html/body/div/div/div[3]/div/div/div[2]/div[1]/div/div/button"));
+			String categoryFilterName = categoryActiveFilter.getText().split(" ")[0];
+			assertEquals(categoryName, categoryFilterName);
+			
+			currentCategory.click();
+			categoryActiveFilter.click();
+		}
+		
+		assertThrows(NoSuchElementException.class, 
+				() -> {webDriver.findElement(By.xpath("//*[@id=\"root\"]/div/div[3]/div/div/div[2]/div[1]/div/div/p"));});
+	}
+	
+	@Test
+	void testSubcategoryFilters() throws InterruptedException {
+		openShopPage();
+		
+		WebElement category = webDriver.findElement(By.className("Category_collapsibleCategory__3wERX"));
+		String categoryName = category.getText().split("\\s+")[0];
+		category.click();
+		
+		List<WebElement> subcategories = webDriver.findElements(By.className("Subcategory_subcategoryItem__1tROv"));
+		
+		for (int i = 0; i < subcategories.size(); i++) {
+			WebElement currentSubcategory = subcategories.get(i);
+			String subcategoryName = currentSubcategory.getText().split("\\s+")[0];
+			String noOfItemsInSubcategoryString = currentSubcategory.getText().split("\\s+")[1];
+			int noOfItemsInSubcategory = Integer.parseInt(noOfItemsInSubcategoryString.replaceAll("[^0-9]", ""));
+			WebElement currentSubcategoryCheckbox = currentSubcategory.findElement(By.tagName("input"));
+			currentSubcategoryCheckbox.click();
+			
+			WebElement categoryActiveFilter = webDriver.findElement(By.xpath("/html/body/div/div/div[3]/div/div/div[2]/div[1]/div/div/button"));
+			String categoryFilterName = categoryActiveFilter.getText().split(" ")[0];
+			assertEquals(categoryName + "/" + subcategoryName, categoryFilterName);
+			
+			if (noOfItemsInSubcategory > 0) {
+				Thread.sleep(2000);
+				List<WebElement> itemsInSubcategory = webDriver.findElements(By.className("Item_itemContainer__uYG6J"));
+				assertEquals(noOfItemsInSubcategory, itemsInSubcategory.size());
+			}
+			
+			categoryActiveFilter.click();
+			assertFalse(currentSubcategoryCheckbox.isSelected());
+		}
+		
+		assertThrows(NoSuchElementException.class, 
+				() -> {webDriver.findElement(By.xpath("//*[@id=\"root\"]/div/div[3]/div/div/div[2]/div[1]/div/div/p"));});
+	}
+	
+	void openShopPage() throws InterruptedException {
+		WebElement shopLink = webDriver.findElement(By.xpath("//*[@id=\"root\"]/div/div[2]/div/div[3]/a[2]"));
+		shopLink.click();
+		Thread.sleep(2000);
 	}
 
 }

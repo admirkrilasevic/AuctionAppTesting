4222a75b5e795815b60b8ab6615349a9eb494a6b
admirkrilasevic <admirkrilasevic@gmail.com>
2022-01-14 11:56:19 +0100

    added multiple filters and clear all test

--- src/test/ShopPageTest.java
+++ src/test/ShopPageTest.java
@@ -12,6 +12,7 @@ import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.NoSuchElementException;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
@@ -161,7 +162,55 @@ class ShopPageTest {
 		Thread.sleep(2000);
 		startingPrice = webDriver.findElement(By.xpath("//*[@id=\"root\"]/div/div[3]/div[2]/div/div/div[2]/p/span"));
 		assertTrue(Double.parseDouble(startingPrice.getText().substring(1)) <= maxPrice);
+	}
+	
+	@Test
+	void testMultipleFiltersAndClearAll() throws InterruptedException {
+		openShopPage();
 		
+		List<WebElement> categories = webDriver.findElements(By.className("Category_collapsibleCategory__3wERX"));
+		String categoryName1 = categories.get(0).getText().split("\\s+")[0];
+		String categoryName2 = categories.get(1).getText().split("\\s+")[0];
+		categories.get(0).click();
+		categories.get(1).click();
+		
+		WebElement subcategory = webDriver.findElement(By.className("Subcategory_subcategoryItem__1tROv"));
+		String subcategoryName = subcategory.getText().split("\\s+")[0];
+		WebElement subcategoryCheckbox = subcategory.findElement(By.tagName("input"));
+		subcategoryCheckbox.click();
+		
+		JavascriptExecutor js = (JavascriptExecutor) webDriver;
+		js.executeScript("window.scrollBy(0,300)", "");
+		Thread.sleep(2000);
+		
+		selectPrices();
+		WebElement minPriceFilterInput = webDriver.findElement(By.className("PriceMenu_minPriceInput__2GC2p"));
+		WebElement maxPriceFilterInput = webDriver.findElement(By.className("PriceMenu_maxPriceInput__2RsAW"));
+		double minPrice = Double.parseDouble(minPriceFilterInput.getAttribute("value"));
+		double maxPrice = Double.parseDouble(maxPriceFilterInput.getAttribute("value"));
+		
+		js.executeScript("window.scrollBy(0,-300)", "");
+		Thread.sleep(2000);
+		
+		List<WebElement> allActiveFilters = webDriver.findElements(By.className("ActiveFilters_filter__xHUuV"));
+		String[] activeFilterNames = new String[3];
+		for (int i = 0; i < allActiveFilters.size(); i++) {
+			activeFilterNames[i] = allActiveFilters.get(i).getText().split(" ")[0];
+		}
+		
+		String expectedCategoryFilterName = categoryName2;
+		String expectedSubcategoryFilterName = categoryName1 + "/" + subcategoryName;
+		String expectedPriceFilterName = "$" + minPrice + "-$" + maxPrice;
+		
+		assertEquals(expectedCategoryFilterName, activeFilterNames[0]);
+		assertEquals(expectedSubcategoryFilterName, activeFilterNames[1]);
+		assertEquals(expectedPriceFilterName, activeFilterNames[2]);
+		
+		WebElement clearAllButton = webDriver.findElement(By.className("ActiveFilters_clearAllButton__3lJdT"));
+		clearAllButton.click();
+		
+		assertThrows(NoSuchElementException.class, 
+				() -> {webDriver.findElement(By.xpath("//*[@id=\"root\"]/div/div[3]/div/div/div[2]/div[1]/div/div/p"));});
 	}
 	
 	void selectPrices() {
